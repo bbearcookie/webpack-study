db6de5f8e53d0aac939bab2780a73a5336e6cd03
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-06-11 16:58:23 +0900

    feat: html-loader 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { titleText } from './title.js';
 import { helloText } from './hello.js';
+import Header from './components/header/header.js';
 import './index.css';
+
+Header();
 
 const $title = document.createElement('h1');
 $title.innerText = helloText + titleText;
