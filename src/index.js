79987ba7edc8819590a4e8a811f54fb42a59740f
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-06-11 16:40:20 +0900

    feat: 스타일 관련 loader 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { titleText } from './title.js';
 import { helloText } from './hello.js';
+import './index.css';
 
 const $title = document.createElement('h1');
 $title.innerText = helloText + titleText;
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'bundles'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
